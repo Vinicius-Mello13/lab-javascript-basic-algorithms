f2a1ff25c5181bdca563a236a96c445a87aac1da
Vinicius Mello <vinicius.dallavecchia@yahoo.com.br>
2022-08-10 11:42:01 -0300

    Aleluia, habemus JS

--- js/index.js
+++ js/index.js
@@ -1,6 +1,39 @@
 // Iteration 1: Names and Input
-//
+const hacker1 = 'Astolfo Jackson'
+console.log("The driver's name is " + hacker1);
+
+const navigator1 = 'Nelson Billie Jean'
+console.log("The navigator's name is " + navigator1)
+
 // Iteration 2: Conditionals
 
 
+console.log(hacker1.length)
+console.log(navigator1.length)
+
+if (navigator1.length > navigator1.length) {
+  console.log("The driver has the longest name, it has " + hacker1.length + " characters")
+} else if 
+  (hacker1.length < navigator1.length) {
+  console.log("It seems that the navigator has the longest name, it has " + navigator1.length + " characters")
+  }
+  else {
+    console.log("Wow, you both have equally long names, " + navigator1.length + " characters! ")
+  }
 // Iteration 3: Loops
+// 3.1
+
+console.log(hacker1.toUpperCase().split("").join(" "))
+
+// 3.2
+console.log(navigator1.split('').reverse().join(''))
+// 3.3
+if (hacker1 > navigator1) {
+  console.log("The driver's name goes first.")
+}
+else if (hacker1 < navigator1) {
+  console.log("Yo, the navigator goes first definitely.")
+}
+else {
+  console.log("What?! You both have the same name?")
+} 
